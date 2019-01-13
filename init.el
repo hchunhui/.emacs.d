0ac51cfc32ed05dd1736e64fea126461c25f0ad4
Chunhui He <hchunhui@mail.ustc.edu.cn>
2019-01-13 08:11:45 +0000

    add lua-mode

--- init.el
+++ init.el
@@ -120,6 +120,10 @@
 (autoload 'rust-mode "rust-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
 
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
 ;; flymake
 (autoload 'flymake-find-file-hook "flymake" "" t)
 (add-hook 'find-file-hook 'flymake-find-file-hook)
