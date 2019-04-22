12a0071f065aec31b97719110a5dae1587e99301
Chunhui He <hchunhui@mail.ustc.edu.cn>
2019-04-22 10:43:42 +0000

    fix flymake

--- init.el
+++ init.el
@@ -130,31 +130,9 @@
 (setq flymake-gui-warnings-enabled nil)
 (setq flymake-log-level 0)
 
-(defun flymake-display-current-error ()
-  "Display errors/warnings under cursor."
-  (interactive)
-  (let ((ovs (overlays-in (point) (1+ (point)))))
-    (catch 'found
-      (dolist (ov ovs)
-        (when (flymake-overlay-p ov)
-          (message (overlay-get ov 'help-echo))
-          (throw 'found t))))))
-
-(defun flymake-goto-next-error-disp ()
-  "Go to next error in err ring, then display error/warning."
-  (interactive)
-  (flymake-goto-next-error)
-  (flymake-display-current-error))
-
-(defun flymake-goto-prev-error-disp ()
-  "Go to previous error in err ring, then display error/warning."
-  (interactive)
-  (flymake-goto-prev-error)
-  (flymake-display-current-error))
-
 (defvar flymake-mode-map (make-sparse-keymap))
-(define-key flymake-mode-map (kbd "<f12>") 'flymake-goto-next-error-disp)
-(define-key flymake-mode-map (kbd "C-<f12>") 'flymake-goto-prev-error-disp)
+(define-key flymake-mode-map (kbd "<f12>") 'flymake-goto-next-error)
+(define-key flymake-mode-map (kbd "C-<f12>") 'flymake-goto-prev-error)
 (or (assoc 'flymake-mode minor-mode-map-alist)
     (setq minor-mode-map-alist
           (cons (cons 'flymake-mode flymake-mode-map)
